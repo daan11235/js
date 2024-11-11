52669c69e44019c1d2665a3b2ac39a4df070e92d
daan11235 <120148436+daan11235@users.noreply.github.com>
2024-11-12 00:42:04 +0100

    Update follow1.js

--- follow1.js
+++ follow1.js
@@ -1,26 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const thumbnails = document.querySelectorAll('[thumbnail-index] .thumbnail');
-    
-    thumbnails.forEach(thumbnail => {
-        const title = thumbnail.querySelector('.caption .page-title');
-        
-        if (title) {
-            thumbnail.addEventListener('mousemove', (event) => {
-                const thumbnailRect = thumbnail.getBoundingClientRect();
-                const offsetX = event.clientX - thumbnailRect.left;
-                const offsetY = event.clientY - thumbnailRect.top;
-                
-                // Position the title relative to mouse movement
-                title.style.position = 'absolute';
-                title.style.left = `${offsetX + 10}px`;
-                title.style.top = `${offsetY + 10}px`;
-                title.style.transform = 'translate(-50%, -50%)';
-            });
+(function(){
+    var mousePositions = [];
 
-            thumbnail.addEventListener('mouseleave', () => {
-                title.style.left = '50%';
-                title.style.top = '50%';
+    var updateMousePositions = function(event) {
+        mousePositions = [];
+        if (event.type === 'touchmove') {
+            _.each(event.touches, function(touch){
+                mousePositions.push({
+                    x: touch.clientX,
+                    y: touch.clientY
+                });
             });
+        } else {
+            mousePositions = [{ x: event.clientX, y: event.clientY }];
         }
-    });
-});
+    };
+
+    var moveTitleWithCursor = function() {
+        var thumbnails = document.querySelectorAll('[thumbnail-index] .thumbnail');
+        
+        thumbnails.forEach(function(thumbnail) {
+            const title = thumbnail.querySelector('.caption .page-title');
+            if (title) {
+                var rect = thumbnail.getBoundingClientRect();
+                var offsetX = mousePositions[0] ? mousePositions[0].x - rect.left : 0;
+                var offsetY = mousePositions[0] ? mousePositions[0].y - rect.top : 0;
+
+                // Move the title based on cursor position
+                title.style.position = 'absolute';
+                title.style.left = `${offsetX + 10}px`;  
+                title.style.top = `${offsetY + 10}px`;  
+                title.style.transform = 'translate(-50%, -50%)';
+            }
+        });
+    };
+
+    window.addEventListener('mousemove', updateMousePositions, { passive: true });
+
+    function updateTitlePosition() {
+        requestAnimationFrame(updateTitlePosition);
+        moveTitleWithCursor();
+    }
+
+    updateTitlePosition();
+})();
